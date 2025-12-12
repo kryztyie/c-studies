19be37e6861ad072f4d6bcb2d01614ccbb6760d6
kay <kayworld39@outlook.com.br>
2025-12-12 18:48:40 -0300

    add: module 03 - countdown_10_to_0.c

---
+++ 03-loops/countdown_10_to_0.c
@@ -0,0 +1,18 @@
+//   Program: Countdown from 10 to 0
+//   Description:
+//   Uses a for loop to iterate from 10
+//   down to 0.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Countdown from 10 to 0\n");
+
+   for (int i = 10; i >= 0; i--){
+       printf("%d\n", i);
+   }
+    
+    return 0;
+}
